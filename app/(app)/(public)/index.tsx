c392899e6024fdfd5e062e1713677e672246cf6c
tahsin005 <tahsin.ferdous3546@gmail.com>
2026-02-12 23:41:27 +0600

    wip: login ui

--- app/(app)/(public)/index.tsx
+++ app/(app)/(public)/index.tsx
@@ -1,6 +1,13 @@
-import { Text, View, StyleSheet, Image } from "react-native";
+import AppleAuthButton from "@/components/auth/AppleAuthButton";
+import GoogleAuthButton from "@/components/auth/GooleAuthButton";
+import { Colors, Fonts } from "@/constants/theme";
+import { Text, View, StyleSheet, Image, TouchableOpacity, Linking } from "react-native";
+import Animated, { FadeInDown } from "react-native-reanimated"
 
 export default function Index() {
+  const openWebBrowser = () => {
+    Linking.openURL('https://tahsin-exe.vercel.app');
+  };
   return (
     <View
       style={styles.container}
@@ -10,6 +17,34 @@ export default function Index() {
       </View>
       <View style={styles.contentContainer}>
         <Image source={require('@/assets/images/wolt-logo.png')} style={styles.brandLogo} />
+        <Animated.Text entering={FadeInDown} style={styles.tagline}>
+          Almost everything delivered
+        </Animated.Text>
+
+        {/* login buttons */}
+        <View style={styles.buttonContainer}>
+          <Animated.View entering={FadeInDown.delay(100)}>
+            <AppleAuthButton />
+          </Animated.View>
+          <Animated.View entering={FadeInDown.delay(200)}>
+            <GoogleAuthButton />
+          </Animated.View>
+          <Animated.View entering={FadeInDown.delay(300)}>
+            <TouchableOpacity style={styles.otherButton}>
+              <Text style={styles.otherButtonText}>Other Options</Text>
+            </TouchableOpacity>
+          </Animated.View>
+        </View>
+
+        <Animated.View style={styles.privacyContainer} entering={FadeInDown.delay(400)}>
+          <Text style={styles.privacyText}>
+            Please visit{' '}
+            <Text style={styles.privacyLink} onPress={openWebBrowser}>
+              Wolt Privacy Statement
+            </Text>{' '}
+            to learn about personal data processing at Wolt.
+          </Text>
+        </Animated.View>
       </View>
     </View>
   );
@@ -21,13 +56,54 @@ const styles = StyleSheet.create({
   },
   contentContainer: {
     flex: 1,
-    alignItems: 'center'
+    alignItems: 'center',
+    paddingHorizontal: 30,
+    paddingVertical: 20,
   },
   brandLogo: {  
     width: '100%',
     height: 48,
     resizeMode: 'contain',
     marginBottom: 20,
+  },
+  tagline: {
+    fontSize: 32,
+    fontFamily: Fonts.brandBlack,
+    textAlign: 'center',
+    marginBottom: 50,
+    lineHeight: 36
+  },
+  buttonContainer: {
+    width: '100%',
+    gap: 12,
+  },
+  otherButton: {
+    backgroundColor: '#f0f0f0',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 17,
+    borderRadius: 12,
+    gap: 4,
+  },
+  otherButtonText: {
+    color: Colors.muted,
+    fontSize: 18,
+    fontWeight: '600',
+  },
+  privacyContainer: {
+    marginTop: 30,
+    paddingHorizontal: 20,
+  },
+  privacyText: {
+    fontSize: 12,
+    color: '#999',
+    textAlign: 'center',
+    lineHeight: 18,
+  },
+  privacyLink: {
+    color: '#4285F4',
+    textDecorationLine: 'underline',
   },
   infiniteScrollContainer: {
     flex: 0.8
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,18 @@
 import { Slot } from "expo-router";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useFonts } from "expo-font";
+import { Nunito_400Regular, Nunito_700Bold, Nunito_900Black } from "@expo-google-fonts/nunito"
 
 const queryClient = new QueryClient();
 
 export default function RootLayout() {
+  let [fontsLoaded] = useFonts({
+    Nunito_400Regular,
+    Nunito_700Bold,
+    Nunito_900Black
+  })
+  if (!fontsLoaded) return null;
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <QueryClientProvider client={queryClient}>
